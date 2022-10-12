db73d0f13e695be1911bc9508c7fe33d9189b183
yaegassy <yaegassy@users.noreply.github.com>
2022-10-12 14:39:32 +0900

    refactor: porting vscode-vue-language-features/src/utils/http.ts

---
+++ src/utils/http.ts
@@ -0,0 +1,27 @@
+import * as http from 'http';
+
+export function isLocalHostPortUsing(port: number) {
+  return new Promise<boolean>((resolve) => {
+    http
+      .get(
+        `http://localhost:${port}/`,
+        {
+          headers: {
+            accept: '*/*', // if not set, always get 404 from vite server
+          },
+        },
+        (res) => {
+          resolve(res.statusCode === 200);
+        }
+      )
+      .on('error', () => resolve(false))
+      .end();
+  });
+}
+
+export async function getLocalHostAvaliablePort(port: number) {
+  if (await isLocalHostPortUsing(port)) {
+    port++;
+  }
+  return port;
+}
