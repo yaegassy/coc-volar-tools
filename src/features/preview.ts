fff6a60f409e1130d586a54894308f3197c38c9c
yaegassy <yaegassy@users.noreply.github.com>
2022-10-24 10:57:57 +0900

    chore: getLocalHostAvaliablePort -> getLocalHostAvailablePort

--- src/features/preview.ts
+++ src/features/preview.ts
@@ -3,7 +3,7 @@ import { parse, SFCParseResult } from '@vue/compiler-sfc';
 import * as coc from 'coc.nvim';
 import * as fs from 'fs';
 import * as path from 'path';
-import { getLocalHostAvaliablePort } from '../utils/http';
+import { getLocalHostAvailablePort } from '../utils/http';
 
 export async function activate(context: coc.ExtensionContext) {
   let ws: ReturnType<typeof preview.createPreviewConnection> | undefined;
@@ -168,7 +168,7 @@ export async function activate(context: coc.ExtensionContext) {
   }
 
   async function startPreviewServer(viteDir: string, type: 'vite' | 'nuxt') {
-    const port = await getLocalHostAvaliablePort(coc.workspace.getConfiguration('volar').get('preview.port')!);
+    const port = await getLocalHostAvailablePort(coc.workspace.getConfiguration('volar').get('preview.port')!);
     const terminal = await coc.window.createTerminal({ name: 'volar-preview:' + port });
     const viteProxyPath =
       type === 'vite'
